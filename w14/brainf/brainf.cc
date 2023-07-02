fd86e4e2416379ad36db83ec4a490fd6067e59af
Luis M Canaval S <public02@canaval.org>
2023-07-02 14:55:47 -0500

    brainf OOP, format and print c code

--- w14/brainf/brainf.cc
+++ w14/brainf/brainf.cc
@@ -1,76 +1,16 @@
-#include <iostream>
-#include <map>
-#include <stack>
-#include <vector>
-// test
+#include "BrainF.hh"
+
 #define MAX 30000
 
-using namespace std;
-
 int main() {
-  unsigned char *c = new unsigned char[MAX];
-  unsigned char *ptr = c;
-  vector<char> program;
-  stack<int> loopbegins;
-  map<int, int> loopmatch;
-  map<int, int> loophctam;
-  char chr;
+	BrainF *bf = new BrainF();
 
-  while ((chr = getchar()) != EOF) {
-    switch (chr) {
-    case '>':
-    case '<':
-    case '+':
-    case '-':
-    case '.':
-    case ',':
-      break;
-    case '[':
-      loopbegins.push(program.size());
-      break;
-    case ']': {
-      auto pos = loopbegins.top();
-      loopbegins.pop();
-      loopmatch[pos] = program.size();
-      loophctam[program.size()] = pos;
-      break;
-    }
-    default:
-      continue;
-    }
-    program.push_back(chr);
-  }
+	bf->parse(std::cin);
+	//bf->run();
+	bf->printC(std::cout);
+	//bf->print(std::cout);
 
-  for (auto i = 0; i < program.size(); ++i) {
-    switch (program[i]) {
-    case '>':
-      ptr++;
-      break;
-    case '<':
-      ptr--;
-      break;
-    case '+':
-      *ptr += 1;
-      break;
-    case '-':
-      *ptr -= 1;
-      break;
-    case '.':
-      cout << *ptr;
-      break;
-    case ',':
-      cin >> *ptr;
-      break;
-    case '[':
-      if (*ptr == 0)
-        i = loopmatch[i];
-      break;
-    case ']':
-      i = loophctam[i] - 1;
-      break;
-    }
-  }
-  cout << endl;
-  delete[] c;
-  return 0;
+	delete bf;
+
+	return 0;
 }
